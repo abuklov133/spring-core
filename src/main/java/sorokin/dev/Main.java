3d3a921f952696d468374b5ff6944dd46e4a327e
abuklov133 <abuklov133@gmail.com>
2025-07-04 20:38:43 +0300

    Changed 1

---
+++ src/main/java/sorokin/dev/Main.java
@@ -0,0 +1,19 @@
+package sorokin.dev;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext("sorokin.dev");
+        Task task = context.getBean(Task.class);
+        Task task1 = (Task) context.getBean("task");
+        System.out.println(task);
+        System.out.println(task1);
+        System.out.println(task == task1);
+        TaskManager taskManager =
+                context.getBean(TaskManager.class);
+        System.out.println(taskManager);
+    }
+}
---
+++ src/main/java/sorokin/dev/TaskManager.java
@@ -0,0 +1,29 @@
+package sorokin.dev;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TaskManager {
+    private final Task task;
+
+    @Autowired
+    public TaskManager(Task task) {
+        this.task = task;
+    }
+
+    public Task getTask() {
+        return task;
+    }
+
+    public void printTask() {
+        System.out.println("Current task " + task.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "TaskManager{" +
+                "task=" + task +
+                '}';
+    }
+}
